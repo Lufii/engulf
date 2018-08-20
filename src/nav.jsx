1e7874201061d92f1966715cf75b7e4d1aaef212
Dani Achim <dani_achim2005@yahoo.com>
2018-08-20 09:37:17 +0300

    Working linkage

--- src/nav.jsx
+++ src/nav.jsx
@@ -11,10 +11,6 @@ class Nav extends Component {
   }
 
 
-  ask(act){
-alert(act);
-return(act)
-}
   render() {
     return (
 <div className='nav'>
@@ -26,19 +22,28 @@ return(act)
   <div className='engulf'>
     <h1>Welcome to Engulf</h1>
     <ul>
-      <li>Log</li>
-      <li>Search</li>
+      <li onClick={() => {
+        this.setState({value: 'log'})
+      }}>Log</li>
+      <li onClick={() => {
+        this.setState({value: 'search'})
+      }}>Search</li>
       <li onClick={() => {
         this.setState({value: 'create'})
-        //alert(this.state.value)
       }}>Create</li>
-      <li>Update</li>
-      <li>Inbound</li>
-      <li>Outbound</li>
+      <li onClick={() => {
+        this.setState({value: 'update'})
+      }}>Update</li>
+      <li onClick={() => {
+        this.setState({value: 'inbound'})
+      }}>Inbound</li>
+      <li onClick={() => {
+        this.setState({value: 'outbound'})
+      }}>Outbound</li>
     </ul>
   </div>
   <br/>
-  <Input />
+  <Input ask={this.state.value}/>
   <br/>
   <Output />
 </div>
@@ -46,22 +51,19 @@ return(act)
 }
 
 class Input extends Component{
-  constructor(props){
-  super(props);
-  this.state={
-    value: null,
-  }
-  }
-
   render(){
+  if(this.props.ask=='create')
     return(
     <div className='search'>
       <input type='text' placeholder="Product Name"></input>
       <button onClick={() => {
-        alert(this.state.value)
+        alert(this.props.thing)
       }} type='submit'>Submit</button>
     </div>
-  )}
+  )
+else {
+  return null
+}}
 }
 
 class Output extends Component{
